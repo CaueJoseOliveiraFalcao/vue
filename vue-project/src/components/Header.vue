f7543336a69e976ddb6c06d9d3e6305747138369
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2025-10-28 13:43:58 -0400

    gotosin

--- vue-project/src/components/Header.vue
+++ vue-project/src/components/Header.vue
@@ -1,18 +1,37 @@
-<script>
+<script setup>
+import { ref } from 'vue';
+let nav = ref('-168px');
+const changeNav = () => {
+    nav.value = nav.value === '-168px' ? '1px' : '-168px';
+}
 </script>
 
 <template>
+        <div class="lateral-bar" :style="{ right: nav}">
+            <button @click="changeNav">volta</button>
+        </div>
         <div class="main">
            <h1>Shop Test</h1>
            
-           <div>
+           <div class="normal-head">
             <router-link to="/home"><a href="#">perfil</a></router-link>
             <router-link to="/carrinho"><a href="#">carrinho</a></router-link>
            </div>
+           <div class="hamburguer">
+            <button @click="changeNav">=</button>
+           </div>
+
         </div>
 </template>
 
 <style scoped>
+    .lateral-bar{
+        position: fixed;
+        background-color: rgb(214, 47, 47);
+        width: 10rem;
+        height: 100%;
+        transition: all 0.3s;
+    }
     .main{
         display: flex;
         padding: 1rem;
@@ -30,12 +49,21 @@
         font-size: 35px;
         color: white;
     }
+    .hamburguer{
+        display: none;
+    }
     @media (max-width : 768px) {
         h1{
             font-size: 25px;
         }
         a{
             font-size: 15px;
+        }
+        .normal-head{
+            display: none;
+        }
+        .hamburguer{
+            display: block;
         }
     }
 </style>
